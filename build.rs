6704d47195fbd0101151cf28c945e13577443e98
themkoi <Pascalsch3@gmail.com>
2025-06-07 15:07:37 +0200

    ui adjustment

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let config =
     slint_build::CompilerConfiguration::new()
-    .with_style("material".into());
+    .with_style("fluent-dark".into());
     slint_build::compile_with_config("ui/app-window.slint",config).expect("Slint build failed");
 }
